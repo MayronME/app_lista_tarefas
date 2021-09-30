545fb5c3413d583175404b9b929e597202ac2127
Mayron <mayron795@gmail.com>
2021-09-30 14:05:37 -0300

    banco de Dados Necessario

---
+++ app_lista_tarefas.sql
@@ -0,0 +1,89 @@
+
+--
+-- Banco de dados: `app_lista_tarefas`
+--
+
+CREATE DATABASE `app_lista_tarefas`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_status`
+--
+
+CREATE TABLE `tb_status` (
+  `id` int(11) NOT NULL,
+  `status` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_status`
+--
+
+INSERT INTO `tb_status` (`id`, `status`) VALUES
+(1, 'pendente'),
+(2, 'realizado');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_tarefas`
+--
+
+CREATE TABLE `tb_tarefas` (
+  `id` int(11) NOT NULL,
+  `id_status` int(11) NOT NULL DEFAULT 1,
+  `tarefa` text NOT NULL,
+  `data_cadastrado` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `tb_tarefas`
+--
+
+INSERT INTO `tb_tarefas` (`id`, `id_status`, `tarefa`, `data_cadastrado`) VALUES
+(16, 1, 'Lavar o Carro', '2021-08-07 10:46:21');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tb_status`
+--
+ALTER TABLE `tb_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `tb_tarefas`
+--
+ALTER TABLE `tb_tarefas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_status` (`id_status`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_status`
+--
+ALTER TABLE `tb_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `tb_tarefas`
+--
+ALTER TABLE `tb_tarefas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `tb_tarefas`
+--
+ALTER TABLE `tb_tarefas`
+  ADD CONSTRAINT `tb_tarefas_ibfk_1` FOREIGN KEY (`id_status`) REFERENCES `tb_status` (`id`);
+COMMIT;
